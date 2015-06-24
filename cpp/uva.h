38f2ac029e0fe58bc9e771d254c07293605bdf66
Stymphalian <saturnslight@gmail.com>
2015-06-24 14:13:35 -0700

    add cpp

---
+++ cpp/uva.h
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <map>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <iostream>
+#define IntVector std::vector<int>
+#define IntPair std::pair<int,int>
