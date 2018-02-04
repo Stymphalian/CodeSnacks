6579b1d063706baade1e4bed40e701a4f24cb10f
Stymphalian <saturnslight@gmail.com>
2018-02-03 16:45:54 -0800

    go install script

---
+++ go/install.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+VERSION=1.9.3
+TMPDIR=/tmp
+
+jajecho Setup golang...
+mkdir -p $TMPDIR/setup_golang/
+
+pushd $TMPDIR/setup_golang
+GOBOOTVERSION=go1.9.3.linux-amd64.tar.gz
+wget https://dl.google.com/go/$GOBOOTVERSION
+echo "a4da5f4c07dfda8194c4621611aeb7ceaab98af0b38bfb29e1be2ebb04c3556c $GOBOOTVERSION" >> go.sha1
+if [[ -n $(sha1sum -c go.sha1 | grep "WARNING") ]]; then
+  echo sha1 hash of file failed. Golang not installed.
+  return
+fi
+tar -xzvf $GOBOOTVERSION
+ln -s $TMPDIR/setup_golang/go ~/go1.4
+popd
+
+# Clone the go source and install
+pushd ~/dev2/local
+git clone https://go.googlesource.com/go
+cd go
+git checkout go$VERSION
+cd src
+./all.bash
+popd
+
+# # remove the bootstrap version
+rm ~/go1.4
